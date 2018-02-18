6e92ae55fc7e1a52b9bc7803ac52cb7e4221d8b1
Anthony Pallone <pallone@unlv.nevada.edu>
2018-02-17 23:14:48 -0800

    added camera and death system

---
+++ Unity/Unity 2d Platformer Tutorial/Assets/playerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerHealth : MonoBehaviour {
+
+    public bool hasDied;
+    public int health;
+
+	// Use this for initialization
+	void Start () {
+        hasDied = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < -7 && !hasDied)
+        {
+            hasDied = true;
+        }
+        if (hasDied){
+            StartCoroutine("Die");
+        }
+	}
+
+    IEnumerator Die ()
+    {
+        SceneManager.LoadScene("prototype area 1");
+        yield return null;
+    }
+}
