97470b52fe968a2f3298e308dec5815f9052656c
Anthony Pallone <pallone@unlv.nevada.edu>
2018-02-25 13:38:20 -0800

    Added first enemy type

---
+++ Unity/Unity 2d Platformer Tutorial/Assets/enemyMoves.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMoves : MonoBehaviour {
+
+    public int enemySpeed = 1;
+    public int xMoveDirection = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0));
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (xMoveDirection, 0) * enemySpeed;
+       if (hit.distance < 0.6f)
+        {
+            flip();
+        }
+    }
+    void flip()
+    {
+        xMoveDirection *= -1;
+    }
+}
