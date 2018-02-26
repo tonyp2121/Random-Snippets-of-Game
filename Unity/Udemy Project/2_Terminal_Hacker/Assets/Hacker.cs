ff6137f3a32dfe92fa9c21ee1c06480416735178
Anthony Pallone <pallone@unlv.nevada.edu>
2018-02-25 22:53:32 -0800

    Added main menu to terminal game

---
+++ Unity/Udemy Project/2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        showMainMenu()
+    }
+	
+    void showMainMenu()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Hello Haxxors, pick a difficulty level");
+        Terminal.WriteLine("1 - Library");
+        Terminal.WriteLine("2 - Police Station");
+        //Terminal.WriteLine("3 - Nasa");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
