b9cfbdca43164544231f0236dbf23ae69206d9fd
Anthony Pallone <pallone@unlv.nevada.edu>
2018-02-28 13:23:22 -0800

    Added thrust

--- Unity/Udemy Project/3_Spaceship_Game/Assets/Rocket.cs
+++ Unity/Udemy Project/3_Spaceship_Game/Assets/Rocket.cs
@@ -8,6 +8,9 @@ public class Rocket : MonoBehaviour {
     Rigidbody rigidBody;
     AudioSource audioSource;
 
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 290f;
+
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
@@ -17,15 +20,18 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput()
+    private void Thrust()
     {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-            if (!audioSource.isPlaying) { 
+            if (!audioSource.isPlaying)
+            {
                 audioSource.Play();
             }
         }
@@ -33,13 +39,23 @@ public class Rocket : MonoBehaviour {
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // before we rotate it we freeze the rotation 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
+
+        rigidBody.freezeRotation = false; // before we rotate it we freeze the rotation 
     }
+
 }
