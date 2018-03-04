b4be944581f966a0f811f937b941afe2a2ad9db0
Anthony Pallone <pallone@unlv.nevada.edu>
2018-03-03 23:43:20 -0800

    Added Player to camera object

---
+++ Unity/Udemy Project/4_Argon_Assault/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    
+    [Tooltip("in m/s")] [SerializeField] float horizontalSpeed = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xOffset = horizontalSpeed * horizontalThrow * Time.deltaTime;
+        print(horizontalThrow);
+	}
+}
