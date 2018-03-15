3b4b4a28ac719370ec9f18da4506dbf623bb4be2
Anthony Pallone <pallone@unlv.nevada.edu>
2018-03-15 14:14:09 -0700

    Added enemy dying with fx playing on death

---
+++ Unity/Udemy Project/4_Argon_Assault/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+	// Use this for initialization
+	void Start () {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnParticleCollision(GameObject particle)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Unity/Udemy Project/4_Argon_Assault/Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour {
+
+    [SerializeField] float timeToDie = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, timeToDie);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
