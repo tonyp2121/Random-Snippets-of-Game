85fafd601b201845b86bfc7ee7ff01266f6e9ec8
Anthony Pallone <pallone@unlv.nevada.edu>
2018-12-03 15:50:51 -0800

    Scoreboard now updates when enemy is destroyed
    Wow holy fuck first commit in whats gotta be 9 months, babies are made
    in the time it last took me to code something even small like this,
    thats crazy. Anyways scoreboard works now

--- Unity/Udemy Project/4_Argon_Assault/Assets/Scripts/Enemy.cs
+++ Unity/Udemy Project/4_Argon_Assault/Assets/Scripts/Enemy.cs
@@ -6,20 +6,31 @@ public class Enemy : MonoBehaviour {
 
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     void OnParticleCollision(GameObject particle)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
---
+++ Unity/Udemy Project/4_Argon_Assault/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+    int score;
+    Text scoreText;
+    [SerializeField] int scorePerHit = 15;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+	}
+	
+    public void ScoreHit(int scorePerHit)
+    {
+        score += scorePerHit;
+        scoreText.text = score.ToString();
+    }
+}
